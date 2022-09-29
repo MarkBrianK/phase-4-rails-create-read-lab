10881f55065891d8f9bdee48396fdd828b8f3e19
MarkBrianK <kariukimarkbrian@gmail.com>
2022-09-29 11:36:04 +0300

    commit tree ecda53f8efa311cee74ab4968b2c2184cd253c31

---
+++ App.js
@@ -0,0 +1,13 @@
+import Header from "./client/src/components/Header";
+import PlantPage from "./client/src/components/PlantPage";
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <PlantPage />
+    </div>
+  );
+}
+
+export default App;
